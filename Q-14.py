c402047d3dde1f27b1c06113c04faa7c79c09881
Ishan Khandelwal <khandelwalishan@rocketmail.com>
2019-04-03 10:46:28 +0530

    Update Q-14.py

--- Q-14.py
+++ Q-14.py
@@ -1,6 +1,5 @@
 f=open("students.txt",'r')
 l=[]
-s=""
 while True:
         n=f.readline()
         if n:
@@ -17,6 +16,17 @@ for i in range(len(l)):
     for j in range(len(l)-1-i):
         if l[j][1]>l[j+1][1]:
             l[j][1],l[j+1][1]=l[j+1][1],l[j][1]
-for i in l:
-    i=tuple(i)
+names=[]
+for i in range(len(l)):
+    if int(l[i][2])<3:
+        names.append(l[i][0])
+d={"CSEE":0,"MME":0,"Biology":0}
+for i in range(len(l)):
+  for j in d:
+      if l[i][3]==j:
+          d[j]+=1
+print(d)
+print(names)
+for i in range(len(l)):
+    l[i]=tuple(l[i])
 print(l)
