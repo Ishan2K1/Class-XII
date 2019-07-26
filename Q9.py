dc784b21591ed00e9822aaade15a3b176649503b
Ishan Khandelwal <khandelwalishan@rocketmail.com>
2019-07-26 10:39:22 +0530

    Update Q9.py

--- Q9.py
+++ Q9.py
@@ -1,12 +1,13 @@
-#Q9 INCOMPLETE
 import numpy as np
-n=np.random.randint(0,50,25)
-print(np.reshape(n,(5,5),order='C'))
-print(np.max(n),"is the max and", np.min(n)," is the min")
-x=float(input("Enter Number To Find Closest To: "))
-y=np.abs(n-x)
-index=np.argmin(y)
-print(n[index])
-Max=n[1]
+n = np.random.randint(10, size=(5,5))
+freq = 0
+print(n)
+print("Maximum value:", np.max(n),"Minimum value:", np.min(n))
 for i in n:
-    if np.bincount(n, weights=i)>np.bincount(Max)    
+    if np.bincount(i).argmax()>freq:
+        freq = np.bincount(i).argmax()
+x = int(input("Enter scalar value: "))
+diff = np.abs(n - x).argmin()
+closest=n.flat[diff]        
+print("Most frequently occuring:", freq)
+print("Closest value to scalar in array:", closest)
